28d992ff482b2a50047a56bb74bc8086aaa17214
Amrit Kooner <amritkooner47@gmail.com>
2023-10-02 20:30:19 +0100

    added some of theme btn functionality and fixed some of mobile responsiveness

--- script/app.js
+++ script/app.js
@@ -1,33 +1,33 @@
 // changes icon state based on toggle
-const btnToggle = (iconElement, classElement, source1, source2) => {
-    classElement.classList.toggle('closedState');
-    iconElement.src = classElement.classList.contains('closedState') ? source1 : source2;
+const btnToggle = (icon, component, source1, source2) => {
+    component.classList.toggle("state");
+    icon.src = component.classList.contains("state") ? source1 : source2;
 };
 
 
 // loads saved state of toggle
-const loadState = (item, classElement, iconElement, source) => {
+const loadState = (icon, component, source, item) => {
     if(localStorage.getItem(item)) {
-        classElement.classList.add("closedState");
-        iconElement.src = source;
+        component.classList.add("state")
+        icon.src = source
     }
 };
 
 
 // burger menu logic
 const burgerMenuLogic = function() {
-    const menuBtn = document.querySelector(".burger-menu-btn");
-    const menu = document.querySelector(".burger-menu");
-    const menuIcon = document.querySelector(".burger-icon");
+    const menuBtn = document.querySelector(".burger-menu-btn")
+    const menu = document.querySelector(".burger-menu")
+    const menuIcon = document.querySelector(".burger-icon")
 
     // loads current state in local storage
-    loadState("burgerMenuState", menu, menuIcon, "img/icons/menu.svg")
+    loadState(menuIcon, menu, "img/icons/menu.svg", "burgerMenuState")
 
     menuBtn.addEventListener("click", () => {
-        btnToggle(menuIcon, menu, "img/icons/menu.svg", "img/icons/x.svg");
+        btnToggle(menuIcon, menu, "img/icons/menu.svg", "img/icons/x.svg")
         
         //updates state in local storage
-        menu.classList.contains("closedState") 
+        menu.classList.contains("state") 
         ? localStorage.setItem("burgerMenuState", "state") 
         : localStorage.removeItem("burgerMenuState")
     });
@@ -36,36 +36,48 @@ const burgerMenuLogic = function() {
 
 // sidebar logic
 const sidebarLogic = function(){
-    const sidebarBtn = document.querySelector(".sidebar-btn");
-    const sidebar = document.querySelector(".sidebar");
-    const sidebarIcon = document.querySelector(".sidebar-icon");
-    const themeBtn = document.querySelector(".theme-btn");
-    const volumeBtn = document.querySelector(".volume-btn");
+    const sidebar = document.querySelector(".sidebar")
+    const mainBody = document.body
+    //
+    const sidebarBtn = document.querySelector(".sidebar-btn")
+    const themeBtn = document.querySelector(".theme-btn")
+    const volumeBtn = document.querySelector(".volume-btn")
+    //
+    const sidebarIcon = document.querySelector(".sidebar-icon")
+    const themeIcon = document.querySelector(".theme-icon")
+    const volumeIcon = document.querySelector(".volume-icon")
 
     // loads current state from local storage
-    loadState("sidebarState", sidebar, sidebarIcon, "img/icons/chevrons-up.svg")
+    loadState(sidebarIcon, sidebar, "img/icons/chevrons-up.svg", "sidebarState")
+    loadState(themeIcon, mainBody, "img/icons/sun.svg", "themeState", "lightTheme")
+    // TODO: LOAD STATE OF VOLUME AFTER FUNCTIONAILITY IS IMPLEMENTED
 
     sidebarBtn.addEventListener("click", () => {
-        btnToggle(sidebarIcon, sidebar, "img/icons/chevrons-up.svg", "img/icons/chevrons-down.svg");
+        btnToggle(sidebarIcon, sidebar, "img/icons/chevrons-up.svg", "img/icons/chevrons-down.svg")
 
         //updates state in local storage
-        sidebar.classList.contains("closedState") 
+        sidebar.classList.contains("state") 
         ? localStorage.setItem("sidebarState", "state") 
         : localStorage.removeItem("sidebarState")
     });
 
-    themeBtn.addEventListener("click", () => {
-        const themeIcon = document.querySelector(".theme-icon");
 
-        btnToggle(themeIcon, themeBtn, "img/icons/sun.svg", "img/icons/moon.svg");
-        // TODO: will have more functionality here
-        // TODO: save state of theme in local storage
+    themeBtn.addEventListener("click", () => {
+        const themeIcon = document.querySelector(".theme-icon")
+
+        btnToggle(themeIcon, mainBody, "img/icons/sun.svg", "img/icons/moon.svg")
+
+        //updates state in local storage
+        mainBody.classList.contains("state") 
+        ? localStorage.setItem("themeState", "theme") 
+        : localStorage.removeItem("themeState")
     });
 
-    volumeBtn.addEventListener("click", () => {
-        const volumeIcon = document.querySelector(".volume-icon");
 
-        btnToggle(volumeIcon, volumeBtn, "img/icons/volume-2.svg", "img/icons/volume-x.svg");
+    volumeBtn.addEventListener("click", () => {
+        const volumeIcon = document.querySelector(".volume-icon")
+
+        // btnToggle(volumeIcon, ???, "img/icons/volume-2.svg", "img/icons/volume-x.svg")
         // TODO: will have more functionality here
         // TODO: save state of volume in local storage
     });
@@ -81,11 +93,11 @@ const slideshowLogic = function(){
     buttons.forEach(btn => {
         btn.addEventListener("click", () => {
             const offset = btn.dataset.sliderBtn === "next" ? 1 : -1;
-            const slides = btn.closest("[data-slider]").querySelector("[data-slides]");
-            const activeSlide = slides.querySelector("[data-active]");
-            const dots = document.querySelectorAll(".pagination .dot");
+            const slides = btn.closest("[data-slider]").querySelector("[data-slides]")
+            const activeSlide = slides.querySelector("[data-active]")
+            const dots = document.querySelectorAll(".pagination .dot")
 
-            let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+            let newIndex = [...slides.children].indexOf(activeSlide) + offset
 
             if (newIndex < 0) {
                 newIndex = slides.children.length - 1;
@@ -93,11 +105,11 @@ const slideshowLogic = function(){
                 newIndex = 0;
             }
         
-            slides.children[newIndex].dataset.active = true;
-            delete activeSlide.dataset.active;
+            slides.children[newIndex].dataset.active = true
+            delete activeSlide.dataset.active
 
             dots.forEach((dot, i) => {
-                dot.classList.toggle("active-dot", i === newIndex);
+                dot.classList.toggle("active-dot", i === newIndex)
             });
         });
     });
@@ -105,7 +117,7 @@ const slideshowLogic = function(){
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    burgerMenuLogic();
-    sidebarLogic();
-    slideshowLogic();
+    burgerMenuLogic()
+    sidebarLogic()
+    slideshowLogic()
 });
